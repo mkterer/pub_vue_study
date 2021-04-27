33ac76bed937f958293e4f6235e1947ba01ef5f2
mkterer <norply>
2021-04-27 17:26:56 +0900

    天気の検索結果表示

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -1,25 +1,55 @@
 <template>
   <div class="weather">
     <h1>This is weather page</h1>
-    <button @click="getWeather">getWeather</button>
-    please look console log    
+    <template v-if="list!==null">
+      <table class="listBox" border="4">
+        <tr>
+          <th>地域</th>
+          <th>最高気温</th>
+          <th>最低気温</th>
+        </tr>
+        <tr v-for="val in list.data[1].tempAverage.areas" :key="val.index">
+          <td>{{ val.area.name }}</td>
+          <td>{{ val.max }}</td>
+          <td>{{ val.min }}</td>
+        </tr>
+      </table>
+    </template>
+
   </div>
 </template>
 
 <script>
 export default ({
+  data() {
+    return {
+      list: null
+    }
+  },
+
   methods: {
+    //天気取得
     getWeather() {
       let url = "https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json"
       this.$axios.get(url)
         .then(res => {
-          console.log(res)
+          this.list = res
         }).catch(err => {
           console.log(err)
         })
     },
+  },
+
+  created () {
+    this.getWeather()
   }
 })
 </script>
+
+<style lang="scss" scoped>
+.listBox {
+  margin: auto;
+}
+</style>
 
 
